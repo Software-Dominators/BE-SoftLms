50dc404eab6d30ec348c657a68665583fff6357d
ArBlackRaven <88971175+ArBlackRaven@users.noreply.github.com>
2024-11-08 15:14:43 +0200

    adding fallback label

--- application/views/frontend/default-new/complains.php
+++ application/views/frontend/default-new/complains.php
@@ -1,44 +1,62 @@
-<div class="d-sm-flex flex-row flex-sm-column justify-content-sm-center flex-md-row justify-content-md-center flex-lg-row justify-content-lg-center flex-xl-row flex-xxl-row" style="background: rgba(0,0,0,0);margin: 10px 0;border-color: var(--bs-secondary-color);">
-        <div style="padding: 0 30px;width: 400px;margin-top: 156px;">
-            <h1>Heading</h1>
-            <p>this is side paragraph to put words in this is the side paragraph to put words in</p>
-        </div>
-        <div style="max-width: 400px;">
-        <pre>
-                <?= var_dump($user_data)?>
-            </pre>
-            <form action="<?php echo base_url('home/put_complains'); ?>" method="POST" style="background: #00000010;padding: 15px 30px;border-radius: 30px;margin: 20px 0;">
-                <div style="margin-bottom: 20px;"><label class="form-label" style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">Name</label><input class="form-control" type="text" name="name" style="font-size: 17px;"></div>
-                <div style="margin-bottom: 20px;"><label class="form-label" style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">Email</label><input class="form-control" type="email" name="email" style="font-size: 17px;"></div>
-                <div style="margin-bottom: 20px;"><label class="form-label" style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">Phone number</label><input class="form-control" type="text" name="phone" style="font-size: 17px;"></div>
-                <div style="margin-bottom: 20px;"><label class="form-label" style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">the course with the problem</label>
-                    <select class="form-select" name="course_id" style="font-size: 17px;">
-                        <option value="0" selected="" hidden>witch course</option>
-                        <?php foreach($courses_data as $course){?>
-                            <option value="<?= $course['id']?>"><?= $course['title']?></option>
-                        <?php }?>
-
-                    </select>
-                </div>
-                <div style="margin-bottom: 20px;"><label class="form-label" style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">the type of the problem</label>
-                    <select class="form-select" name="problem_type" style="font-size: 17px;" >
-                        <?php if($user_data['role_id'] == 1){?>
-                            <option value="" selected="" hidden>What is the type of the problem</option>
-                            <option value="sign_problem">Sign Problem</option>
-                            <option value="payment_problem">Payment Problem</option>
-                            <option value="general_matter">General matter</option>
-                            <?php } else {?>
-                                <option value="" selected="" hidden>What is the type of the problem</option>
-                                <option value="technical_problem">Technical Problem</option>
-                                <option value="quiz_problem">Quiz Problem</option>
-                                <option value="content_problem">Content Problem</option>
-                                <option value="general_problem">General Problem</option>
-                        <?php }?>
-                    </select>
-                </div>
-                <div style="margin-bottom: 20px;"><label class="form-label" style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">attatch a screenshot&nbsp;</label><input class="form-control" type="file" style="font-size: 17px;"></div>
-                <div style="margin-bottom: 20px;"><label class="form-label" style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">what else you want add ...</label><textarea class="form-control" name="message" style="font-size: 17px;"></textarea></div>
-                <div class="d-lg-flex justify-content-lg-center"><input class="btn btn-primary" type="submit" style="margin: auto;padding: 8px 50px;border-radius: 30px;"></div>
-            </form>
-        </div>
+<div class="d-sm-flex flex-row flex-sm-column justify-content-sm-center flex-md-row justify-content-md-center flex-lg-row justify-content-lg-center flex-xl-row flex-xxl-row"
+    style="background: rgba(0,0,0,0);margin: 10px 0;border-color: var(--bs-secondary-color);">
+    <div style="padding: 0 30px;width: 400px;margin-top: 156px;">
+        <h1>Heading</h1>
+        <p>this is side paragraph to put words in this is the side paragraph to put words in</p>
     </div>
+    <div style="max-width: 400px;">
+        <pre>
+
+            </pre>
+        <form action="<?php echo base_url('home/put_complains'); ?>" method="POST"
+            style="background: #00000010;padding: 15px 30px;border-radius: 30px;margin: 20px 0;">
+            <div style="margin-bottom: 20px;"><label class="form-label"
+                    style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">Name</label><input
+                    class="form-control" type="text" name="name" style="font-size: 17px;"></div>
+            <div style="margin-bottom: 20px;"><label class="form-label"
+                    style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">Email</label><input
+                    class="form-control" type="email" name="email" style="font-size: 17px;"></div>
+            <div style="margin-bottom: 20px;"><label class="form-label"
+                    style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">Phone
+                    number</label><input class="form-control" type="text" name="phone" style="font-size: 17px;"></div>
+            <div style="margin-bottom: 20px;"><label class="form-label"
+                    style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">the course with
+                    the problem</label>
+                <select class="form-select" name="course_id" style="font-size: 17px;">
+                    <option value="0" selected="" hidden>witch course</option>
+                    <?php foreach ($courses_data as $course) { ?>
+                        <option value="<?= $course['id'] ?>"><?= $course['title'] ?></option>
+                    <?php } ?>
+
+                </select>
+            </div>
+            <div style="margin-bottom: 20px;"><label class="form-label"
+                    style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">the type of the
+                    problem</label>
+                <select class="form-select" name="problem_type" style="font-size: 17px;">
+                    <?php if ($user_data['role_id'] == 1) { ?>
+                        <option value="" selected="" hidden>What is the type of the problem</option>
+                        <option value="sign_problem">Sign Problem</option>
+                        <option value="payment_problem">Payment Problem</option>
+                        <option value="general_matter">General matter</option>
+                    <?php } else { ?>
+                        <option value="" selected="" hidden>What is the type of the problem</option>
+                        <option value="technical_problem">Technical Problem</option>
+                        <option value="quiz_problem">Quiz Problem</option>
+                        <option value="content_problem">Content Problem</option>
+                        <option value="general_problem">General Problem</option>
+                    <?php } ?>
+                </select>
+            </div>
+            <div style="margin-bottom: 20px;"><label class="form-label"
+                    style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">attatch a
+                    screenshot&nbsp;</label><input class="form-control" type="file" style="font-size: 17px;"></div>
+            <div style="margin-bottom: 20px;"><label class="form-label"
+                    style="margin-bottom: 0;padding-left: 10px;color: var(--bs-light-text-emphasis);">what else you want
+                    add ...</label><textarea class="form-control" name="message" style="font-size: 17px;"></textarea>
+            </div>
+            <div class="d-lg-flex justify-content-lg-center"><input class="btn btn-primary" type="submit"
+                    style="margin: auto;padding: 8px 50px;border-radius: 30px;"></div>
+        </form>
+    </div>
+</div>
--- application/views/frontend/default-new/course_page_curriculum.php
+++ application/views/frontend/default-new/course_page_curriculum.php
@@ -1,25 +1,29 @@
 <div class="accordion curriculum-accordion mx-2">
     <?php
     $sections = $this->crud_model->get_section('course', $course_id)->result_array();
-    foreach ($sections as $key => $section) : ?>
-    <!-- Accordion Area -->
+    foreach ($sections as $key => $section): ?>
+        <!-- Accordion Area -->
         <div class="accordion-item">
             <h2 class="accordion-header mx-2">
-                <button class="accordion-button <?php if($key > 0) echo 'collapsed'; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#curriculumSectionCol<?php echo $section['id']; ?>" aria-expanded="false" aria-controls="curriculumSectionCol<?php echo $section['id']; ?>">
+                <button class="accordion-button <?php if ($key > 0)
+                    echo 'collapsed'; ?>" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#curriculumSectionCol<?php echo $section['id']; ?>" aria-expanded="false"
+                    aria-controls="curriculumSectionCol<?php echo $section['id']; ?>">
                     <div class="row w-100">
                         <div class="col-auto accordion-item-title d-flex flex-column">
-                            
+
 
                             <!-- Study plan start-->
-                            <?php if(date('d-M-Y-H-i-s', $section['start_date']) != date('d-M-Y-H-i-s', $section['end_date'])): ?>
+                            <?php if (date('d-M-Y-H-i-s', $section['start_date']) != date('d-M-Y-H-i-s', $section['end_date'])): ?>
                                 <span style="margin-top: -10px;"><?php echo $section['title']; ?></span>
-                                <small class="text-12px text-muted mt-1" data-bs-toggle="tooltip" title="<?php echo get_phrase('Study plan') ?>">
+                                <small class="text-12px text-muted mt-1" data-bs-toggle="tooltip"
+                                    title="<?php echo get_phrase('Study plan') ?>">
                                     <i class="far fa-calendar-alt"></i>
-                                    <?php if(date('d-M-Y', $section['start_date']) == date('d-M-Y', $section['end_date'])): ?>
-                                         <?php echo date('d M Y', $section['start_date']); ?>:
-                                         <?php echo date('h:i A', $section['start_date']).' - '.date('h:i A', $section['end_date']); ?>
+                                    <?php if (date('d-M-Y', $section['start_date']) == date('d-M-Y', $section['end_date'])): ?>
+                                        <?php echo date('d M Y', $section['start_date']); ?>:
+                                        <?php echo date('h:i A', $section['start_date']) . ' - ' . date('h:i A', $section['end_date']); ?>
                                     <?php else: ?>
-                                        <?php echo date('d M Y h:i A', $section['start_date']).' - '.date('d M Y h:i A', $section['end_date']); ?>
+                                        <?php echo date('d M Y h:i A', $section['start_date']) . ' - ' . date('d M Y h:i A', $section['end_date']); ?>
                                     <?php endif ?>
                                 </small>
                             <?php else: ?>
@@ -39,31 +43,44 @@
                     </div>
                 </button>
             </h2>
-            <div id="curriculumSectionCol<?php echo $section['id']; ?>" class="accordion-collapse collapse <?php if($key == 0) echo 'show'; ?>" data-bs-parent="#curriculumSection<?php echo $section['id']; ?>">
+            <div id="curriculumSectionCol<?php echo $section['id']; ?>" class="accordion-collapse collapse <?php if ($key == 0)
+                   echo 'show'; ?>" data-bs-parent="#curriculumSection<?php echo $section['id']; ?>">
                 <div class="accordion-body p-0">
                     <ul class="ac-lecture">
                         <?php $lessons = $this->crud_model->get_lessons('section', $section['id'])->result_array();
-                        foreach ($lessons as $lesson) : ?>
+                        foreach ($lessons as $lesson): ?>
                             <li>
-                                <a href="#" onclick="actionTo('<?php echo site_url('home/play_lesson/'.$lesson['id']); ?>')" class="checkPropagation">
+                                <a href="#" onclick="actionTo('<?php echo site_url('home/play_lesson/' . $lesson['id']); ?>')"
+                                    class="checkPropagation">
                                     <span class="d-flex align-items-center ellipsis-line-2">
                                         <i class="fa-regular fa-circle-play"></i>
                                         <?php echo $lesson['title']; ?>
                                     </span>
 
-                                    <div>
+                                    <?php if ($lesson['lesson_type'] == 'quiz'): ?>
                                         <div>
-                                            <span>start date:  <span style="color:#6610f2;" class="fw-bold"><?php echo date('Y-m-d',$lesson['start_time']) ?></span> || </span>
-                                            <span>end date:  <span style="color:#6610f2;" class="fw-bold"><?php echo date('Y-m-d',$lesson['end_time']) ?></span> </span>
+                                            <div>
+                                                <span>start date: <span style="color:#6610f2;"
+                                                        class="fw-bold"><?php echo date('Y-m-d', $lesson['start_time']) ?></span> ||
+                                                </span>
+                                                <span>end date: <span style="color:#6610f2;"
+                                                        class="fw-bold"><?php echo date('Y-m-d', $lesson['end_time']) ?></span>
+                                                </span>
+                                            </div>
+                                            <div style="text-align:center;">
+                                                <span>duration: <span
+                                                        style="color:#6610f2;"><?php echo $lesson['duration'] ?></span></span>
+                                            </div>
                                         </div>
-                                        <div style="text-align:center;">
-                                            <span>duration:  <span style="color:#6610f2;"><?php echo $lesson['duration'] ?></span></span>
-                                        </div>
-                                    </div>
+                                    <?php endif; ?>
 
-                                    <?php if($lesson['is_free']): ?>
+                                    <?php if ($lesson['is_free']): ?>
                                         <div class="lecture-info ms-auto pe-2 me-2">
-                                            <span onclick="lesson_preview('<?php echo site_url('home/play_lesson/'.$lesson['id'].'/preview') ?>', '<?php echo $lesson['title']; ?>', 'lg')" class="checkPropagation cursor-pointer badge bg-light text-dark fw-400 text-13px"><i class="fas fa-eye me-1 text-13px"></i> <?php echo get_phrase('Preview') ?></span>
+                                            <span
+                                                onclick="lesson_preview('<?php echo site_url('home/play_lesson/' . $lesson['id'] . '/preview') ?>', '<?php echo $lesson['title']; ?>', 'lg')"
+                                                class="checkPropagation cursor-pointer badge bg-light text-dark fw-400 text-13px"><i
+                                                    class="fas fa-eye me-1 text-13px"></i>
+                                                <?php echo get_phrase('Preview') ?></span>
                                         </div>
                                     <?php endif; ?>
 
