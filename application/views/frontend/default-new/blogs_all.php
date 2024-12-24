e30ee2142923b93c3001e2b3fc1afc0a31a716bf
Tasneem_Ahmed <tasneemamed1999@gmail.com>
2024-12-24 22:30:47 +0200

    blogs

--- application/views/frontend/default-new/blogs_all.php
+++ application/views/frontend/default-new/blogs_all.php
@@ -1,19 +1,19 @@
 <section class="blogs">
     <div class="container">
         <div class="row">
-        <div class="col-12">
-                        <p class="blogs__total  ">
-                            <?php if (isset($search_string) || isset($_GET['category'])):
-                                echo site_phrase('total') . ' ' . $total_rows . ' ' . site_phrase('results');
-                            else:
-                                echo site_phrase('total') . ' ' . $total_rows . ' ' . site_phrase('articles');
-                            endif; ?>
-                        </p>
-                    </div>
+            <div class="col-12">
+                <p class="blogs__total">
+                    <?php if (isset($search_string) || isset($_GET['category'])):
+                        echo site_phrase('total') . ' ' . $total_rows . ' ' . site_phrase('results');
+                    else:
+                        echo site_phrase('total') . ' ' . $total_rows . ' ' . site_phrase('articles');
+                    endif; ?>
+                </p>
+            </div>
             <?php foreach ($blogs->result_array() as $blog): ?>
                 <?php $user_details = $this->user_model->get_all_user($blog['user_id'])->row_array(); ?>
-                <div class="col-lg-4 col-md-2 col-12">
-                    <div class="blogs__content">
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="blogs__content d-flex flex-column h-100">
                         <div class="blogs__img">
                             <?php $blog_thumbnail = 'uploads/blog/thumbnail/' . $blog['thumbnail']; ?>
                             <?php if (file_exists($blog_thumbnail) && is_file($blog_thumbnail)): ?>
@@ -36,42 +36,29 @@
                                 <?php echo ellipsis(strip_tags(htmlspecialchars_decode_($blog['description'])), 150); ?>
                             </p>
                         </div>
-                        <div class="blogs__bottom d-flex justify-content-between flex-md-row flex-column bg-info ">
+                        <div class="blogs__bottom mt-auto   d-flex  justify-content-between flex-md-row flex-column">
                             <div class="blogs__creator d-flex align-items-center ">
                                 <img loading="lazy" class="rounded-circle"
                                     src="<?php echo $this->user_model->get_user_image_url($user_details['id']); ?>">
-                                <h5><?php echo $user_details['first_name'] . ' ' . $user_details['last_name']; ?>
-                                </h5>
+                                <h5 class="ms-2"><?php echo $user_details['first_name'] . ' ' . $user_details['last_name']; ?></h5>
                             </div>
-
-
-                            <a class="blogs__reade-more text-end"
-                            href="<?php echo site_url('blog/details/' . slugify($blog['title']) . '/' . $blog['blog_id']); ?>">
-                              
-                                <?= get_phrase('Read More') ?>
-                                <i class="fa-solid fa-arrow-right"></i>
-                            </a>
-
+                            <div class="blogs__read-more text-end">
+                                <a class="text-end"
+                                    href="<?php echo site_url('blog/details/' . slugify($blog['title']) . '/' . $blog['blog_id']); ?>">
+                                    <?= get_phrase('Read More') ?>
+                                    <i class="fa-solid fa-arrow-right"></i>
+                                </a>
+                            </div>
                         </div>
-
-
-
-
                     </div>
                 </div>
             <?php endforeach; ?>
 
             <div class="col-12 text-center">
-                        <nav><?php echo $this->pagination->create_links(); ?></nav>
-                    </div>
+                <nav><?php echo $this->pagination->create_links(); ?></nav>
+            </div>
         </div>
     </div>
-
 </section>
-
-
-
-
-
 
 <?php include "blog_recent.php"; ?>
