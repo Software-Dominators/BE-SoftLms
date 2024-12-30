dc8efdad0fe258ed91c53b0c439be806eec45102
Tasneem_Ahmed <tasneemamed1999@gmail.com>
2024-12-30 22:12:19 +0200

    remove

--- application/views/frontend/default-new/includes_top.php
+++ application/views/frontend/default-new/includes_top.php
@@ -32,15 +32,3 @@
 <script src="<?php echo base_url('assets/global/js/jquery-3.6.1.min.js'); ?>"></script>
 
 
-<!-- ########################################design one links ################# -->
- <!-- Swiper -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-<!-- Font Awesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
-    integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- design one links -->
-<link rel="stylesheet" href="<?php echo base_url() . 'assets/frontend/design-one/assets/css/import.css'; ?>">
-
